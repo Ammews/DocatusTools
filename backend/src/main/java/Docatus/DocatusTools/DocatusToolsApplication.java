d15eb5438ddd5b8de51ec3003dc7ab5cb1073adc
Pedro Ammes <pedro.ammes@gmail.com>
2025-07-21 11:04:17 -0300

    Inicialização do projeto
    Base do projeto criada

---
+++ backend/src/main/java/Docatus/DocatusTools/DocatusToolsApplication.java
@@ -0,0 +1,13 @@
+package Docatus.DocatusTools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocatusToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocatusToolsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Docatus/DocatusTools/DocatusToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package Docatus.DocatusTools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocatusToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
