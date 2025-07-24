b4f1d9df058dbae755970a0091771e9e02ec14ec
Pedro Ammes <pedro.ammes@gmail.com>
2025-07-24 16:13:25 -0300

    Adicionado ads
    Adicionado ads

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,32 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { QrcodeComponent } from './pages/qrcode/qrcode.component';
+import { ArquivosComponent } from './pages/arquivos/arquivos.component';
+import { LinksComponent } from './pages/links/links.component';
+import { CurriculosComponent } from './pages/curriculos/curriculos.component';
 
 export const routes: Routes = [
 
     {
         path:"",
         component: HomeComponent
+    },
+    {
+        path:"qrcode",
+        component: QrcodeComponent
+    },
+    {
+        path:"arquivos",
+        component: ArquivosComponent
+    },
+    {
+        path:"links",
+        component: LinksComponent
+    },
+    {
+        path:"curriculo",
+        component: CurriculosComponent
     }
+
 
 ];
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { QRCodeComponent } from 'angularx-qrcode';
 import { QrcodeGenComponent } from "../../components/qrcode-gen/qrcode-gen.component";
 import { ServicesComponent } from '../../components/services/services.component';
 import { CardComponent } from '../../components/card/card.component';
+declare var adsbygoogle: any;
 
 @Component({
   selector: 'app-home',
@@ -12,5 +13,12 @@ import { CardComponent } from '../../components/card/card.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  ngAfterViewInit(): void {
+  try {
+    (window as any).adsbygoogle = (window as any).adsbygoogle || [];
+    (window as any).adsbygoogle.push({});
+  } catch (e) {
+    console.error('Erro ao carregar anúncio:', e);
+  }
+}
 }
